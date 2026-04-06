355953df0d5ca7360df8c0c67e10e745a4798c85
Mark Horrocks <mr.horrocks@gmail.com>
2026-04-06 02:34:52 +0100

    Sass(css) added to app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  css: ['~/assets/scss/main.scss'],
   modules: ['@nuxtjs/tailwindcss']
 })
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          'Arial'
+        ],
+        serif: ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+        mono: [
+          'ui-monospace',
+          'SFMono-Regular',
+          'Menlo',
+          'Monaco',
+          'Consolas',
+          '"Liberation Mono"',
+          '"Courier New"',
+          'monospace',
+        ],
+      },
+    },
+  },
+  plugins: [],
+}
